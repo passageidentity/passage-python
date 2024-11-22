1e763699b00400074f866a4959b62770a50bc60b
Chris Tran <chris.tran@agilebits.com>
2024-11-22 20:59:04 +0000

    style: fixes test file linter errors

--- tests/authenticate_test.py
+++ tests/authenticate_test.py
@@ -1,224 +1,233 @@
-from passageidentity.passage import Passage
-from passageidentity import PassageError
-from faker import Faker
-import pytest
 import os
+from typing import cast
+
+import pytest
 from dotenv import load_dotenv
+from faker import Faker
+
+from passageidentity import PassageError
+from passageidentity.openapi_client.models.app_info import AppInfo
+from passageidentity.openapi_client.models.create_user_request import CreateUserRequest
+from passageidentity.openapi_client.models.update_user_request import UpdateUserRequest
+from passageidentity.openapi_client.models.user_info import UserInfo
+from passageidentity.openapi_client.models.web_authn_devices import WebAuthnDevices
+from passageidentity.passage import Passage
 
 load_dotenv()
 f = Faker()
-PASSAGE_USER_ID = os.environ.get("PASSAGE_USER_ID")
-PASSAGE_APP_ID = os.environ.get("PASSAGE_APP_ID")
-PASSAGE_API_KEY = os.environ.get("PASSAGE_API_KEY")
-PASSAGE_AUTH_TOKEN = os.environ.get("PASSAGE_AUTH_TOKEN")
+PASSAGE_USER_ID = os.environ.get("PASSAGE_USER_ID") or ""
+PASSAGE_APP_ID = os.environ.get("PASSAGE_APP_ID") or ""
+PASSAGE_API_KEY = os.environ.get("PASSAGE_API_KEY") or ""
+PASSAGE_AUTH_TOKEN = os.environ.get("PASSAGE_AUTH_TOKEN") or ""
 
-
-def randomPhone():
-    return "+1512" + "".join(
-        str(f.random_number(digits=1, fix_len=True)) for _ in range(7)
-    )
-
-
-def testValidJWT():
+def test_valid_jwt() -> None:
     psg = Passage(PASSAGE_APP_ID, auth_strategy=Passage.HEADER_AUTH)
     user = psg.authenticateJWT(PASSAGE_AUTH_TOKEN)
     assert user == PASSAGE_USER_ID
 
 
-def testInvalidJWT():
+def test_invalid_jwt() -> None:
     psg = Passage(PASSAGE_APP_ID, auth_strategy=Passage.HEADER_AUTH)
     with pytest.raises(PassageError):
         psg.authenticateJWT("invalid_token")
 
 
-def testValidateJWT():
+def test_validate_jwt() -> None:
     psg = Passage(PASSAGE_APP_ID, auth_strategy=Passage.HEADER_AUTH)
     user = psg.validateJwt(PASSAGE_AUTH_TOKEN)
     assert user == PASSAGE_USER_ID
 
 
-def testFetchJWKS():
+def test_fetch_jwks() -> None:
     psg = Passage(PASSAGE_APP_ID, auth_strategy=Passage.HEADER_AUTH)
     assert len(psg.jwks) > 0
 
 
-def testGetApp():
+def test_get_app() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
-    app = psg.getApp()
-    assert PASSAGE_APP_ID == app.id
+    app = cast(AppInfo, psg.getApp())
+    assert app.id == PASSAGE_APP_ID
 
 
-def testCreateMagicLink():
-    magicLinkAttributes = {
+def test_create_magic_link() -> None:
+    psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
+    magic_link = psg.createMagicLink({
         "email": "chris@passage.id",
         "channel": "email",
         "ttl": 12,
-    }
-    psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
-    magicLink = psg.createMagicLink(magicLinkAttributes)
-    assert magicLink.identifier == "chris@passage.id"
-    assert magicLink.ttl == 12
+    }) # type: ignore[arg-type]
+    assert magic_link.identifier == "chris@passage.id" # type: ignore[attr-defined]
+    assert magic_link.ttl == 12 # type: ignore[attr-defined]
 
 
-def testGetUserInfoValid():
+def test_get_user_info_valid() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
-    user = psg.getUser(PASSAGE_USER_ID)
+    user = cast(UserInfo, psg.getUser(PASSAGE_USER_ID))
     assert user.id == PASSAGE_USER_ID
 
 
-def testGetUserInfoByIdentifierValid():
+def test_get_user_info_by_identifier_valid() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     email = f.email()
-    newUser = psg.createUser({"email": email})
-    assert newUser.email == email
+    req = CreateUserRequest(email=email)
+    new_user = cast(UserInfo, psg.createUser(req))
+    assert new_user.email == email
 
-    userByIdentifier = psg.getUserByIdentifier(email)
-    assert userByIdentifier.id == newUser.id
+    user_by_identifier = cast(UserInfo, psg.getUserByIdentifier(email))
+    assert user_by_identifier.id == new_user.id
 
-    user = psg.getUser(newUser.id)
-    assert user.id == newUser.id
+    user = cast(UserInfo, psg.getUser(new_user.id))
+    assert user.id == new_user.id
 
-    assert userByIdentifier == user
-    assert psg.deleteUser(newUser.id)
+    assert user_by_identifier == user
+    assert psg.deleteUser(new_user.id)
 
 
-def testGetUserInfoByIdentifierValidUpperCase():
+def test_get_user_info_by_identifier_valid_upper_case() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     email = f.email()
-    newUser = psg.createUser({"email": email})
-    assert newUser.email == email
+    req = CreateUserRequest(email=email)
+    new_user = cast(UserInfo, psg.createUser(req))
+    assert new_user.email == email
 
-    userByIdentifier = psg.getUserByIdentifier(email.upper())
-    assert userByIdentifier.id == newUser.id
+    user_by_identifier = cast(UserInfo, psg.getUserByIdentifier(email.upper()))
+    assert user_by_identifier.id == new_user.id
 
-    user = psg.getUser(newUser.id)
-    assert user.id == newUser.id
+    user = cast(UserInfo, psg.getUser(new_user.id))
+    assert user.id == new_user.id
 
-    assert userByIdentifier == user
-    assert psg.deleteUser(newUser.id)
+    assert user_by_identifier == user
+    assert psg.deleteUser(new_user.id)
 
 
-def testGetUserInfoByIdentifierPhoneValid():
+def test_get_user_info_by_identifier_phone_valid() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
-    phone = randomPhone()
-    newUser = psg.createUser({"phone": phone})
-    assert newUser.phone == phone
+    phone = "+15005550030"
+    req = CreateUserRequest(phone=phone)
+    new_user = cast(UserInfo, psg.createUser(req))
+    assert new_user.phone == phone
 
-    userByIdentifier = psg.getUserByIdentifier(phone)
-    assert userByIdentifier.id == newUser.id
+    user_by_identifier = cast(UserInfo, psg.getUserByIdentifier(phone))
+    assert user_by_identifier.id == new_user.id
 
-    user = psg.getUser(newUser.id)
-    assert user.id == newUser.id
+    user = cast(UserInfo, psg.getUser(new_user.id))
+    assert user.id == new_user.id
 
-    assert userByIdentifier == user
-    assert psg.deleteUser(newUser.id)
+    assert user_by_identifier == user
+    assert psg.deleteUser(new_user.id)
 
 
-def testGetUserInfoByIdentifierError():
+def test_get_user_info_by_identifier_error() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     with pytest.raises(PassageError, match="Failed to find user data"):
         psg.getUserByIdentifier("error@passage.id")
 
 
-def testActivateUser():
+def test_activate_user() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
-    user = psg.activateUser(PASSAGE_USER_ID)
+    user = cast(UserInfo, psg.activateUser(PASSAGE_USER_ID))
     assert user.status == "active"
 
 
-def testDeactivateUser():
+def test_deactivate_user() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
-    user = psg.getUser(PASSAGE_USER_ID)
-    user = psg.deactivateUser(user.id)
+    user = cast(UserInfo, psg.getUser(PASSAGE_USER_ID))
+    user = cast(UserInfo, psg.deactivateUser(user.id))
     assert user.status == "inactive"
 
 
-# def testListUserDevices():
-#     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
-
-#     devices = psg.listUserDevices(PASSAGE_USER_ID)
-#     assert len(devices) == 2
-
-# listUserDevices & revokeUserDevice are not tested because it is impossible to spoof webauthn to create a device to then revoke
-
-
-def testUpdateUserPhone():
+def test_list_user_devices() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
-    phone1 = randomPhone()
-    user = psg.updateUser(PASSAGE_USER_ID, {"phone": phone1})
-    assert user.phone == phone1
+    devices = cast(list[WebAuthnDevices], psg.listUserDevices(PASSAGE_USER_ID))
+    assert len(devices) == 2
 
 
-def testUpdateUserEmail():
+
+def test_update_user_phone() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
-    email1 = f.email()
-    user = psg.updateUser(PASSAGE_USER_ID, {"email": email1})
-    assert user.email == email1
+    phone = "+15005550021"
+    req = CreateUserRequest(phone=phone)
+    new_user = cast(UserInfo, psg.createUser(req))
+
+    phone = "+15005550022"
+    req = UpdateUserRequest(phone=phone)
+    user = cast(UserInfo, psg.updateUser(new_user.id, req))
+    assert user.phone == phone
+    assert psg.deleteUser(new_user.id)
 
 
-def testUpdateUserWithMetadata():
+def test_update_user_email() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     email = f.email()
-    user = psg.updateUser(
-        PASSAGE_USER_ID, {"email": email, "user_metadata": {"example1": "qwe"}}
-    )
+    req = UpdateUserRequest(email=email)
+    user = cast(UserInfo, psg.updateUser(PASSAGE_USER_ID, req))
     assert user.email == email
-    assert user.user_metadata["example1"] == "qwe"
-
-    user = psg.updateUser(
-        PASSAGE_USER_ID, {"email": email, "user_metadata": {"example1": "asd"}}
-    )
-    assert user.email == email
-    assert user.user_metadata["example1"] == "asd"
 
 
-def testCreateUserWithMetadata():
+def test_update_user_with_metadata() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     email = f.email()
-    user = psg.createUser({"email": email, "user_metadata": {"example1": "qwe"}})
+    req = UpdateUserRequest(email=email, user_metadata={"example1": "qwe"})
+    user = cast(UserInfo, psg.updateUser(PASSAGE_USER_ID, req))
     assert user.email == email
-    assert user.user_metadata["example1"] == "qwe"
+    assert user.user_metadata["example1"] == "qwe" # type: ignore[index]
+
+    req = UpdateUserRequest(email=email, user_metadata={"example1": "asd"})
+    user = cast(UserInfo, psg.updateUser(PASSAGE_USER_ID, req))
+    assert user.email == email
+    assert user.user_metadata["example1"] == "asd" # type: ignore[index]
+
+
+def test_create_user_with_metadata() -> None:
+    psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
+
+    email = f.email()
+    req = CreateUserRequest(email=email, user_metadata={"example1": "qwe"})
+    user = cast(UserInfo, psg.createUser(req))
+    assert user.email == email
+    assert user.user_metadata["example1"] == "qwe" # type: ignore[index]
     assert psg.deleteUser(user.id)
 
 
-def testGetUserInfoUserDoesNotExist():
+def test_get_user_info_user_does_not_exist() -> None:
     pass
 
 
-def testCreateAndDeleteUser():
+def test_create_and_delete_user() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     email = f.email()
-    newUser = psg.createUser({"email": email})
-    assert newUser.email == email
-    assert psg.deleteUser(newUser.id)
+    req = CreateUserRequest(email=email)
+    new_user = cast(UserInfo, psg.createUser(req))
+    assert new_user.email == email
+    assert psg.deleteUser(new_user.id)
 
 
-def testSmartLinkValid():
+def test_smart_link_valid() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     email = f.email()
-    magicLink = psg.createMagicLink({"email": email})
-    assert magicLink.identifier == email
-    assert not magicLink.activated
+    magic_link = psg.createMagicLink({"email": email}) # type: ignore[arg-type]
+    assert magic_link.identifier == email # type: ignore[attr-defined]
+    assert not magic_link.activated # type: ignore[attr-defined]
 
 
-def testsignOut():
+def testsign_out() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     assert psg.signOut(PASSAGE_USER_ID)
 
 
-def testrevokeUserRefreshTokens():
+def testrevoke_user_refresh_tokens() -> None:
     psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
     assert psg.revokeUserRefreshTokens(PASSAGE_USER_ID)
