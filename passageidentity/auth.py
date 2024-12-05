39badc6599fbad5f958ccbcf7b73b973a2105b1c
Chris Tran <chris.tran@agilebits.com>
2024-12-05 23:10:26 +0000

    feat: updates create_magic_link method to use stronger typed parameters

--- passageidentity/auth.py
+++ passageidentity/auth.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 
 import jwt
 import jwt.algorithms
@@ -11,12 +11,49 @@ from passageidentity.errors import PassageError
 from passageidentity.helper import fetch_app
 from passageidentity.openapi_client.api.magic_links_api import MagicLinksApi
 from passageidentity.openapi_client.exceptions import ApiException
-from passageidentity.openapi_client.models.create_magic_link_request import CreateMagicLinkRequest
 
 if TYPE_CHECKING:
     from passageidentity.openapi_client.models.magic_link import MagicLink
+    from passageidentity.openapi_client.models.magic_link_channel import MagicLinkChannel
+    from passageidentity.openapi_client.models.magic_link_type import MagicLinkType
 
-CreateMagicLinkArgs = CreateMagicLinkRequest
+
+class MagicLinkArgsBase:
+    """Base class for MagicLinkArgs."""
+
+    type: MagicLinkType
+    send: bool
+
+
+class MagicLinkWithEmailArgs(MagicLinkArgsBase):
+    """Arguments for creating a Magic Link with an email."""
+
+    email: str
+
+
+class MagicLinkWithPhoneArgs(MagicLinkArgsBase):
+    """Arguments for creating a Magic Link with a phone number."""
+
+    phone: str
+
+
+class MagicLinkWithUserArgs(MagicLinkArgsBase):
+    """Arguments for creating a Magic Link with a user ID."""
+
+    user_id: str
+    channel: MagicLinkChannel
+
+
+MagicLinkArgs = Union[MagicLinkWithEmailArgs, MagicLinkWithPhoneArgs, MagicLinkWithUserArgs]
+
+
+class MagicLinkOptions:
+    """Options for creating a Magic Link."""
+
+    language: str | None
+    magic_link_path: str | None
+    redirect_url: str | None
+    ttl: int | None
 
 
 class Auth:
@@ -52,31 +89,34 @@ class Auth:
             msg = f"JWT is not valid: {e}"
             raise PassageError(msg) from e
 
-    def create_magic_link(self, args: CreateMagicLinkArgs) -> MagicLink:
+    def create_magic_link(self, args: MagicLinkArgs, options: MagicLinkOptions | None = None) -> MagicLink:
         """Create a Magic Link for your app."""
-        magic_link_req = {}
-        args_dict = args.to_dict() if isinstance(args, CreateMagicLinkRequest) else args
+        payload = {
+            "type": args.type,
+            "send": args.send,
+        }
 
-        magic_link_req["user_id"] = args_dict.get("user_id") or ""
-        magic_link_req["email"] = args_dict.get("email") or ""
-        magic_link_req["phone"] = args_dict.get("phone") or ""
+        if isinstance(args, MagicLinkWithEmailArgs):
+            payload["email"] = args.email
+        elif isinstance(args, MagicLinkWithPhoneArgs):
+            payload["phone"] = args.phone
+        elif isinstance(args, MagicLinkWithUserArgs):
+            payload["user_id"] = args.user_id
+            payload["channel"] = args.channel
+        else:
+            msg = "args must be an instance of MagicLinkArgs"
+            raise TypeError(msg)
 
-        magic_link_req["language"] = args_dict.get("language") or ""
-        magic_link_req["magic_link_path"] = args_dict.get("magic_link_path") or ""
-        magic_link_req["redirect_url"] = args_dict.get("redirect_url") or ""
-        magic_link_req["send"] = args_dict.get("send") or False
-        magic_link_req["ttl"] = args_dict.get("ttl") or 0
-        magic_link_req["type"] = args_dict.get("type") or "login"
-
-        if args_dict.get("email"):
-            magic_link_req["channel"] = args_dict.get("channel") or "email"
-        elif args_dict.get("phone"):
-            magic_link_req["channel"] = args_dict.get("channel") or "phone"
+        if options:
+            payload["language"] = options.language
+            payload["magic_link_path"] = options.magic_link_path
+            payload["redirect_url"] = options.redirect_url
+            payload["ttl"] = options.ttl
 
         try:
             return self.magic_links_api.create_magic_link(
                 self.app_id,
-                magic_link_req,  # type: ignore[arg-type]
+                payload,  # type: ignore[arg-type]
                 _headers=self.request_headers,
             ).magic_link
         except ApiException as e:
